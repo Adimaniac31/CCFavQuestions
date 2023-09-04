3c20d87af959e47fde302987c452ccc9cfb624c1
adimaniac <adityabhigade31@gmail.com>
2023-09-05 00:13:22 +0530

    add a btn when click saves link to goBtn

--- popup.js
+++ popup.js
@@ -1,2 +1,16 @@
 const AddBtn = document.getElementById("Add");
 const GoBtn = document.getElementById("GoBtn");
+const GoBtnLink = document.getElementById("GoBtnLink");
+
+AddBtn.addEventListener("click",function(){
+    chrome.scripting.executeScript({
+        target: {tabId: tabID},
+        function: () => {
+            const link = window.location.href;
+            chrome.storage.local.set({"copiedLink": link}, function(){
+            GoBtnLink.href = link;
+            GoBtnLink.removeAttribute("disabled");
+            });
+        },
+    });
+})
