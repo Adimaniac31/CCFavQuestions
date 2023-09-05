0c800bc264f3a4754392b1a55b852b1cb5b7f8c6
adimaniac <adityabhigade31@gmail.com>
2023-09-05 22:17:39 +0530

    delete everything

--- popup.js
+++ popup.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const AddBtn = document.getElementById("Add");
-    const GoBtnLink = document.getElementById("GoBtnLink");
-  
-    AddBtn.addEventListener("click", function () {
-      // Get the link from the popup's URL
-      const link = window.location.href;
-  
-      // Store the link in local storage
-      chrome.storage.local.set({ "copiedLink": link }, function () {
-        // Set the "Go" button's href and enable it
-        GoBtnLink.href = link;
-        GoBtnLink.removeAttribute("disabled");
-      });
+  const AddBtn = document.getElementById("Add");
+  const GoBtnLink = document.getElementById("GoBtnLink");
+
+  AddBtn.addEventListener("click", function () {
+    // Get the link from the popup's URL
+    const link = window.location.href;
+
+    // Store the link in local storage
+    chrome.storage.local.set({ "copiedLink": link }, function () {
+      // Set the "Go" button's href and enable it
+      GoBtnLink.href = link;
+      GoBtnLink.removeAttribute("disabled");
     });
   });
+});
+
   
