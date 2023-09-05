d80965939fff0b70fd77492032bbf3a1f98e6c89
adimaniac <adityabhigade31@gmail.com>
2023-09-05 22:56:43 +0530

    add functionality to AddBtn

--- popup.js
+++ popup.js
@@ -2,8 +2,11 @@ const AddBtn = document.getElementById("Add");
 const GoBtnLink = document.getElementById("GoBtnLink");
 const GoBtn = document.getElementById("GoBtn");
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
+AddBtn.addEventListener('click',function(){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        console.log(tabs[0].url);
+    });
 });
+
 
   
