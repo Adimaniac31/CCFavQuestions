1dfd6ed95f707cd7b2de18621ea11f9189bd0574
adimaniac <adityabhigade31@gmail.com>
2023-09-05 22:55:18 +0530

    define AddBtn,GoBtnLink,GoBtn

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+const AddBtn = document.getElementById("Add");
+const GoBtnLink = document.getElementById("GoBtnLink");
+const GoBtn = document.getElementById("GoBtn");
+
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     console.log(tabs[0].url);
 });
