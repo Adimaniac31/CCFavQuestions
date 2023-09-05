6d72cb6f6d6ba56f1796a2b2ecd8361ee6125057
adimaniac <adityabhigade31@gmail.com>
2023-09-05 22:19:54 +0530

    add tabs to permission

--- popup.js
+++ popup.js
@@ -1,18 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const AddBtn = document.getElementById("Add");
-  const GoBtnLink = document.getElementById("GoBtnLink");
 
-  AddBtn.addEventListener("click", function () {
-    // Get the link from the popup's URL
-    const link = window.location.href;
-
-    // Store the link in local storage
-    chrome.storage.local.set({ "copiedLink": link }, function () {
-      // Set the "Go" button's href and enable it
-      GoBtnLink.href = link;
-      GoBtnLink.removeAttribute("disabled");
-    });
-  });
-});
 
   
